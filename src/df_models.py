9bf06ca11832ecd9e38ee18f9d9da0d203f90543
amine <amine.bouamama@mandel.tech>
2023-02-09 08:05:37 +0100

    poc

---
+++ src/df_models.py
@@ -0,0 +1,53 @@
+from abc import ABC
+from typing import Literal
+
+import pandas as pd
+from pandera import DataFrameSchema, Column, Check
+
+
+class DataframeModel(ABC):
+    schema: DataFrameSchema
+    name: str
+    type: Literal["csv", "xlsx"] = "csv"
+
+    def load(self) -> pd.DataFrame:
+        # cache management can be done here
+
+        if self.type == 'csv':
+            return self.schema.validate(
+                pd.read_csv(f"data/{self.name}.{self.type}")
+            )
+        elif self.type == 'xlsx':
+            return self.schema.validate(
+                pd.read_excel(f"data/{self.name}.{self.type}")
+            )
+        raise ValueError(f"Unknown type {self.type}")
+
+    def save(self, df: pd.DataFrame) -> None:
+        if self.type == 'csv':
+            self.schema.validate(df).to_csv(f"data/{self.name}.{self.type}")
+            return
+        elif self.type == 'xlsx':
+            self.schema.validate(df).to_excel(f"data/{self.name}.{self.type}")
+            return
+        raise ValueError(f"Unknown type {self.type}")
+
+
+class InputDataModel(DataframeModel):
+    schema = DataFrameSchema(
+        {
+            "contractId": Column(int),
+            "engagement": Column(float, checks=[Check.greater_than_or_equal_to(0)], nullable=False, coerce=True),
+            "categorie": Column(str, nullable=False, checks=[Check.isin(["A", "B", "C"])]),
+        }
+    )
+    name = "input_data"
+    type = "csv"
+
+
+class OutputDataModel(DataframeModel):
+    schema = InputDataModel.schema.add_columns({
+        "engagement_doubled": Column(float, checks=[Check.greater_than_or_equal_to(0)], nullable=False)
+    })
+    name = "output_data"
+    type = "xlsx"
---
+++ src/main.py
@@ -0,0 +1,18 @@
+from src.df_models import InputDataModel, OutputDataModel
+
+
+def run() -> None:
+    print('Hello, World!')
+
+    # 1. load initial data
+    data = InputDataModel().load()
+
+    # 2. process data
+    data["engagement_doubled"] = data["engagement"] * 2
+
+    # 3. save data
+    OutputDataModel().save(df=data)
+
+
+if __name__ == '__main__':
+    run()
